c645c112fa7c815aefa94737fc6aea98c4fc4442
dylan <dylanalvarez1995@gmail.com>
2019-04-06 14:01:16 -0300

    Implement sleep and cpu endpoints in python

--- py/app.py
+++ py/app.py
@@ -1,10 +1,27 @@
+import time
+
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def ping():
     return 'Hello, World!'
+
+
+@app.route('/sleep/')
+def timeout():
+    time.sleep(1)
+    return 'Slept for 1 second'
+
+
+@app.route('/cpu/')
+def cpu():
+    result = 1
+    for i in range(10000000):
+        result += i / result
+    return f'Result: {result}'
 
 
 if __name__ == "main":
